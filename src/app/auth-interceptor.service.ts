a6f9c83d96ff4a20f0435bcaa93f5060ca68a6cc
Mohd Salim <mohd.salim@soprasteria.com>
2020-05-03 16:20:12 +0530

    Manipulating request objects

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -6,7 +6,12 @@ export class AuthInterceptorService implements HttpInterceptor {
     // it runs right before our http requests leave our application
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('Request is on its way');
-        return next.handle(req);
+        //req.url = "adad"; // not possible url is a immutable object
+        const modifiedRequest = req.clone(
+            {headers: req.headers.append('Auth', 'xyz')}
+        );
+        //return next.handle(req);
+        return next.handle(modifiedRequest);
     }
     
 }
