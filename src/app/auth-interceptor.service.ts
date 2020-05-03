69726a145ee24e645ea560c5600abd809892ce1b
Mohd Salim <mohd.salim@soprasteria.com>
2020-05-03 21:21:22 +0530

    Response interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
-import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpEventType } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
 
@@ -11,7 +12,18 @@ export class AuthInterceptorService implements HttpInterceptor {
             {headers: req.headers.append('Auth', 'xyz')}
         );
         //return next.handle(req);
-        return next.handle(modifiedRequest);
+        //return next.handle(modifiedRequest);
+
+        // Response can be manipulated here also..
+        return next.handle(modifiedRequest).pipe(
+            tap(event => {
+                console.log(event);
+                if(event.type === HttpEventType.Response) {
+                    console.log('Response arrived, body data: ');
+                    console.log(event.body);
+                }
+            })
+        );
     }
     
 }
